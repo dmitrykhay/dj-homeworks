6d0e015ac5c7503f308b83f805b2745a3473b472
Dmitry Khayretdinov <dmitrykhay@icloud.com>
2023-04-27 15:15:32 +0300

    HW 7.2

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,6 @@
+import csv
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -7,11 +10,20 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
+    bus_stations = []
+    with open(settings.BUS_STATION_CSV,  newline='', encoding='utf8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            station = {}
+            station['Name'] = row['Name']
+            station['Street'] = row['Street']
+            station['District'] = row['District']
+            bus_stations.append(station)
+    paginator = Paginator(bus_stations, 20)
+    page_number = int(request.GET.get('page', 1))
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,13 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipes(request, dish):
+    servings = int(request.GET.get('servings', 1))
+    recipe = DATA[dish]
+    context = {'recipe': {}}
+    for ingredient, quantity in recipe.items():
+        context['recipe'][ingredient] = round(quantity * servings, 2)
+    return render(request, 'calculator/index.html', context)
